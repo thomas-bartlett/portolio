e109713087f512a9ff15e390114619cb2fc2575c
Thomas Bartlett <67928676+thomas-bartlett@users.noreply.github.com>
2024-05-23 11:27:16 +0100

    fix the github action

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,14 @@ export default function Home() {
     <main>
       <h1>Thomas's Portfolio</h1>
       <p>A portfolio of Thomas's work</p>
+      <Image
+     src="/portfolio/vercel.svg"
+     alt="Vercel Logo"
+     className={styles.vercelLogo}
+     width={100}
+     height={24}
+     priority
+   />
     </main>
   );
 }
