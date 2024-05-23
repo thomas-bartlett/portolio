7305a3b6d2c17b70d7c8be580224d5fcd8740cb8
Thomas Bartlett <67928676+thomas-bartlett@users.noreply.github.com>
2024-05-23 11:32:46 +0100

    get minimal build working

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,6 @@ export default function Home() {
     <main>
       <h1>Thomas&apos;s Portfolio</h1>
       <p>A portfolio of Thomas&apos;s work</p>
-      <Image
-     src="/portfolio/vercel.svg"
-     alt="Vercel Logo"
-     className={styles.vercelLogo}
-     width={100}
-     height={24}
-     priority
-   />
     </main>
   );
 }
