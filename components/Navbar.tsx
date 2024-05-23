2de901b7614039009627421fd92c4cb76b496fc9
Thomas Bartlett <67928676+thomas-bartlett@users.noreply.github.com>
2024-05-23 11:04:42 +0100

    initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/about">About</a>
+            </li>
+            <li>
+                <a href="/projects">Projects</a>
+            </li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
