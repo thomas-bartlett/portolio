4e8b209dd749372585450de5b4bdb96ba1fa5790
Thomas Bartlett <67928676+thomas-bartlett@users.noreply.github.com>
2024-05-23 12:56:21 +0100

    fix error

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,18 @@ export default function Home() {
       <h1 className="text-5xl font-bold mb-4">Thomas&apos;s Portfolio</h1>
       <p className="mb-8 text-lg">Welcome to my portfolio. This is a work in progress. I hope to have an outline of my work as well as a blog to share some helpful info.</p>
 
-      <div class="flex flex-col gap-4 w-full">
-        <div class="skeleton h-32 w-full"></div>
-        <div class="skeleton h-4 w-8/12"></div>
-        <div class="skeleton h-4 w-full"></div>
-        <div class="skeleton h-4 w-full"></div>
-        <div class="skeleton h-4 w-5/12"></div>
-        <div class="skeleton h-4 w-full"></div>
-        <div class="skeleton h-4 w-full"></div>
-        <div class="skeleton h-4 w-full"></div>
-        <div class="skeleton h-4 w-5/12"></div>
-        <div class="skeleton h-4 w-full"></div>
-        <div class="skeleton h-4 w-full"></div>
+      <div className="flex flex-col gap-4 w-full">
+        <div className="skeleton h-32 w-full"></div>
+        <div className="skeleton h-4 w-8/12"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-5/12"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-5/12"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
       </div>  
     </div>
   );
